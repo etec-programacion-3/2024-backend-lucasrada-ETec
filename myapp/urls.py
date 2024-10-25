e51a0d4992693701777aab7876099be4c5d5d490
Matias-F-Diaz-C <mfc.diaz@alumno.etec.um.edu.ar>
2024-10-25 12:55:24 -0300

    Crud Finalizado creo

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path
-from . import views
+from .views import (
+    ProductListCreateView, ProductDetailView,
+    MusicProdDetailsListCreateView, MusicProdDetailsDetailView,
+    OrderDetailsListCreateView, OrderDetailsDetailView,
+    # Include other views as needed...
+)
 
 urlpatterns = [
-    path ("", views.home ,name="home")
+    # Product URLs
+    path('products/', ProductListCreateView.as_view(), name='product-list-create'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+
+    # MusicProdDetails URLs
+    path('music-products/', MusicProdDetailsListCreateView.as_view(), name='musicprod-list-create'),
+    path('music-products/<int:pk>/', MusicProdDetailsDetailView.as_view(), name='musicprod-detail'),
+
+    # OrderDetails URLs
+    path('orders/', OrderDetailsListCreateView.as_view(), name='order-list-create'),
+    path('orders/<int:pk>/', OrderDetailsDetailView.as_view(), name='order-detail'),
+
+    # Add URLs for other models...
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,75 @@
-from django.shortcuts import render, HttpResponse
+from rest_framework import generics
+from .models import Product, MusicProdDetails, AudioProdDetails, OrderDetails, OrderItems, User, UserPayment, MusicDiscography
+from .serializers import ProductSerializer, MusicProdDetailsSerializer, AudioProdDetailsSerializer, OrderDetailsSerializer, OrderItemsSerializer, UserSerializer, UserPaymentSerializer, MusicDiscographySerializer
 
-# Create your views here.
-def home(request): 
-    return HttpResponse("Hello World")
+# Product Views
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# MusicProdDetails Views
+class MusicProdDetailsListCreateView(generics.ListCreateAPIView):
+    queryset = MusicProdDetails.objects.all()
+    serializer_class = MusicProdDetailsSerializer
+
+class MusicProdDetailsDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MusicProdDetails.objects.all()
+    serializer_class = MusicProdDetailsSerializer
+
+# AudioProdDetails Views
+class AudioProdDetailsListCreateView(generics.ListCreateAPIView):
+    queryset = AudioProdDetails.objects.all()
+    serializer_class = AudioProdDetailsSerializer
+
+class AudioProdDetailsDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = AudioProdDetails.objects.all()
+    serializer_class = AudioProdDetailsSerializer
+
+# OrderDetails Views
+class OrderDetailsListCreateView(generics.ListCreateAPIView):
+    queryset = OrderDetails.objects.all()
+    serializer_class = OrderDetailsSerializer
+
+class OrderDetailsDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = OrderDetails.objects.all()
+    serializer_class = OrderDetailsSerializer
+
+# OrderItems Views
+class OrderItemsListCreateView(generics.ListCreateAPIView):
+    queryset = OrderItems.objects.all()
+    serializer_class = OrderItemsSerializer
+
+class OrderItemsDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = OrderItems.objects.all()
+    serializer_class = OrderItemsSerializer
+
+# User Views
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# UserPayment Views
+class UserPaymentListCreateView(generics.ListCreateAPIView):
+    queryset = UserPayment.objects.all()
+    serializer_class = UserPaymentSerializer
+
+class UserPaymentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UserPayment.objects.all()
+    serializer_class = UserPaymentSerializer
+
+# MusicDiscography Views
+class MusicDiscographyListCreateView(generics.ListCreateAPIView):
+    queryset = MusicDiscography.objects.all()
+    serializer_class = MusicDiscographySerializer
+
+class MusicDiscographyDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MusicDiscography.objects.all()
+    serializer_class = MusicDiscographySerializer
